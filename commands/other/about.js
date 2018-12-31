3dada46ad83c833f05151ba8a1443f8aa73dc7f8
EnforcedPizza <46079606+EnforcedPizza@users.noreply.github.com>
2018-12-31 08:07:47 -0700

    Update about.js

--- commands/other/about.js
+++ commands/other/about.js
@@ -6,11 +6,11 @@ class about extends commando.Command {
             name: 'about',
             group: 'other',
             memberName: 'about',
-            description: 'This is a bot made by YOU!'
+            description: 'This is a bot made by CallMehMiles#0807!'
         });
     }
 async run(message, args){
-    message.reply("This bot was made by @YOU made with Visual Studio Code and Node.JS");
+    message.reply("This bot was made by CallMehMiles#0807 made with Atom and Discord Bot Maker.);
 }
 
 }
